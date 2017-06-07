0dba1624b3f6330f06ea279c8afd9837e3054bb2
Dvir Hazut <dvir.hazut@kaltura.com>
2017-06-07 23:58:48 +0200

    Icon library component so the other component will have nicer and more semantic JSX code. removed actions.js after all actions were separated into the reducers files.

--- src/components/volume/volume.js
+++ src/components/volume/volume.js
@@ -4,6 +4,7 @@ import { connect } from 'preact-redux';
 import { bindActions } from '../../utils/bind-actions';
 import { actions } from '../../reducers/volume';
 import BaseComponent from '../base';
+import Icon from '../icon/icon';
 
 const mapStateToProps = state => ({
   isDraggingActive: state.volume.isDraggingActive,
@@ -15,14 +16,12 @@ const mapStateToProps = state => ({
 class VolumeControl extends BaseComponent {
   _volumeControlElement: HTMLElement;
   _volumeProgressBarElement: HTMLElement;
-  _playerElement: HTMLElement;
 
   constructor(obj: IControlParams) {
     super({name: 'Volume', player: obj.player});
   }
 
   componentDidMount() {
-    this._playerElement = document.getElementsByClassName('player')[0];
     this._volumeControlElement = document.getElementsByClassName('volume-control')[0];
     this._volumeProgressBarElement = this._volumeControlElement.getElementsByClassName('bar')[0];
 
@@ -32,19 +31,6 @@ class VolumeControl extends BaseComponent {
 
     this.player.addEventListener(this.player.Event.VOLUME_CHANGE, () => {
       this.props.updateVolume(this.player.volume);
-    });
-
-    this._playerElement.addEventListener('mousemove', (e) => {
-      if (this.props.isDraggingActive) {
-        this.changeVolume(e);
-      }
-    });
-
-    this._playerElement.addEventListener('mouseup', (e) => {
-      if (this.props.isDraggingActive) {
-        this.changeVolume(e);
-        this.props.updateVolumeDraggingStatus(false);
-      }
     });
   }
 
@@ -97,16 +83,9 @@ class VolumeControl extends BaseComponent {
       return (
         <div className={controlButtonClass}>
           <button className='control-button' onClick={() => this.onVolumeControlButtonClick()} aria-label='Volume'>
-            <svg viewBox='0 0 1024 1024'>
-              <path d='M224 352l234.504-156.336c29.545-19.697 53.496-7.194 53.496 28.053v576.566c0 35.19-24.059 47.677-53.496 28.053l-234.504-156.336h-127.906c-17.725 0-32.094-14.581-32.094-31.853v-256.295c0-17.592 14.012-31.853 32.094-31.853h127.906zM288 637.748l160 106.667v-464.83l-160 106.667v251.496zM128 416v192h96v-192h-96z' />
-            </svg>
-            <svg className='volume-waves' viewBox='0 0 1024 1024'>
-              <path d='M802.017 837.177c82.359-86.627 129.183-201.774 129.183-324.26 0-123.976-47.976-240.409-132.127-327.329-12.293-12.697-32.552-13.025-45.249-0.732s-13.025 32.552-0.732 45.249c72.692 75.084 114.109 175.597 114.109 282.812 0 105.928-40.422 205.331-111.566 280.162-12.177 12.808-11.666 33.063 1.143 45.24s33.063 11.666 45.24-1.143z' />
-              <path d='M667.436 743.221c67.761-60.884 107.273-147.888 107.273-241.233 0-87.318-34.552-169.203-94.836-229.446-12.501-12.493-32.762-12.486-45.255 0.015s-12.486 32.762 0.015 45.255c48.375 48.342 76.075 113.989 76.075 184.176 0 75.021-31.679 144.776-86.048 193.627-13.146 11.812-14.227 32.044-2.416 45.19s32.044 14.227 45.19 2.416z' />
-            </svg>
-            <svg className='volume-mute' viewBox='0 0 1024 1024'>
-              <path d='M768 466.745l-67.986-67.986c-12.213-12.213-32.654-12.393-45.151 0.104-12.584 12.584-12.543 32.711-0.104 45.151l67.986 67.986-67.986 67.986c-12.213 12.213-12.393 32.654 0.104 45.151 12.584 12.584 32.711 12.543 45.151 0.104l67.986-67.986 67.986 67.986c12.213 12.213 32.654 12.393 45.151-0.104 12.584-12.584 12.543-32.711 0.104-45.151l-67.986-67.986 67.986-67.986c12.213-12.213 12.393-32.654-0.104-45.151-12.584-12.584-32.711-12.543-45.151-0.104l-67.986 67.986z' />
-            </svg>
+            <Icon type='volume-base' />
+            <Icon type='volume-waves' />
+            <Icon type='volume-mute' />
           </button>
           <div className='volume-control-bar'>
             <div className='bar' onMouseDown={() => this.onVolumeProgressBarMouseDown()} onClick={e => this.onVolumeProgressBarClick(e)}>
