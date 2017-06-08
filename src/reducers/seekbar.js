0a525eb04adbd596a225339428d4ca5aaff28acc
Dvir Hazut <dvir.hazut@kaltura.com>
2017-06-08 10:11:25 +0200

    virtual progress removed from store as for overhead of events on mouse move. managed in internal component state now.

--- src/reducers/seekbar.js
+++ src/reducers/seekbar.js
@@ -2,14 +2,12 @@
 
 export const types = {
   UPDATE_SEEKBAR_DRAGGING_STATUS: 'seekbar/UPDATE_SEEKBAR_DRAGGING_STATUS',
-  UPDATE_SEEKBAR_VIRTUAL_PROGRESS: 'seekbar/UPDATE_SEEKBAR_VIRTUAL_PROGRESS',
   UPDATE_CURRENT_TIME: 'seekbar/UPDATE_CURRENT_TIME',
   UPDATE_DURATION: 'seekbar/UPDATE_DURATION'
 }
 
 export const initialState = {
 	currentTime: 0,
-  virtualTime: 0,
   duration: 0,
   draggingActive: false
 }
@@ -20,12 +18,6 @@ export default (state = initialState, action) => {
       return {
         ...state,
         draggingActive: action.draggingActive
-      }
-
-    case types.UPDATE_SEEKBAR_VIRTUAL_PROGRESS:
-      return {
-        ...state,
-        virtualTime: action.virtualTime
       }
 
     case types.UPDATE_CURRENT_TIME:
@@ -48,6 +40,5 @@ export default (state = initialState, action) => {
 export const actions = {
   updateSeekbarDraggingStatus: (draggingActive: boolean) => ({ type: types.UPDATE_SEEKBAR_DRAGGING_STATUS, draggingActive }),
   updateDuration: (duration: number) => ({ type: types.UPDATE_DURATION, duration }),
-  updateVirtualTime: (virtualTime: number) => ({ type: types.UPDATE_SEEKBAR_VIRTUAL_PROGRESS, virtualTime }),
   updateCurrentTime: (currentTime: number) => ({ type: types.UPDATE_CURRENT_TIME, currentTime })
 }
