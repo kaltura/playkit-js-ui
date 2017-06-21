28ed539a5c18734d9153f54c3e3dc2fadfc438a0
Dvir Hazut <dvir.hazut@kaltura.com>
2017-06-21 14:28:39 +0200

    cvaa overlay

--- src/components/cvaa-overlay/cvaa-overlay.js
+++ src/components/cvaa-overlay/cvaa-overlay.js
@@ -18,13 +18,16 @@ class CVAAOverlay extends BaseComponent {
 
   render(props) {
     return !props.open ? '' : (
-      <Overlay type='cvaa'>
+      <Overlay open={props.open} onClose={() => props.toggleCVAAOverlay(false)} type='cvaa'>
         <div className='title'>
           Advanced captions settings
         </div>
-        <div className='sample'>Sample</div>
-        <div className='sample black-bg'>Sample</div>
-        <div className='sample yellow-text'>Sample</div>
+        <div>
+          <div className='sample'>Sample</div>
+          <div className='sample black-bg'>Sample</div>
+          <div className='sample yellow-text'>Sample</div>
+        </div>
+        <a className='button-save-cvaa'>Set custom caption</a>
       </Overlay>
     )
   }
