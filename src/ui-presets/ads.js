2c086bb9636df8495906be8ad41025b4902e59a7
Dvir Hazut <dvir.hazut@kaltura.com>
2017-09-04 11:03:29 +0200

    Removing the player ads UI as the IMA plugin adds UI and it can’t be removed.

--- src/ui-presets/ads.js
+++ src/ui-presets/ads.js
@@ -6,9 +6,6 @@ import SeekBarAdsContainer from '../components/seekbar-ads-container';
 import VolumeControl from '../components/volume';
 import FullscreenControl from '../components/fullscreen';
 import TimeDisplayPlaybackContainer from '../components/time-display-playback-container';
-import AdSkip from '../components/ad-skip';
-import AdLearnMore from '../components/ad-learn-more';
-import TopBar from '../components/top-bar';
 import BottomBar from '../components/bottom-bar';
 
 /**
@@ -19,32 +16,10 @@ import BottomBar from '../components/bottom-bar';
  * @returns {HTMLElement} player ui tree
  */
 export default function adsUI(props: any): React$Element<any> {
-  var useStyledLinearAds = false;
-
-  try {
-    useStyledLinearAds = props.player.config.plugins.ima.adsRenderingSettings.useStyledLinearAds;
-  } catch (e) {
-    //TODO: add error handling
-  }
-
   return (
     <div className='ad-gui-wrapper'>
       <Loading player={props.player} />
       <div className='player-gui' id='player-gui'>
-        {
-          useStyledLinearAds ? undefined :
-          <div>
-            <TopBar>
-              <div className='left-controls'>
-                <span className='font-size-base'>Adverisment</span>
-              </div>
-              <div className='right-controls'>
-                <AdLearnMore />
-              </div>
-            </TopBar>
-            <AdSkip player={props.player} />
-          </div>
-        }
         <BottomBar>
           <SeekBarAdsContainer adBreak showFramePreview showTimeBubble player={props.player} />
           <div className='left-controls'>
