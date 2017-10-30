d2d3f1256266f424afaa060737c54b14b1e460d9
odedhutzler <oded.hutzler@kaltura.com>
2017-10-30 14:45:06 +0200

    adding loader to ads - fix

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -103,6 +103,7 @@ class Loading extends BaseComponent {
 
     this.player.addEventListener(this.player.Event.AD_LOADED, () => {
       this.props.updateLoadingSpinnerState(true);
+    });
 
     this.player.addEventListener(this.player.Event.AD_STARTED, () => {
       this.props.updateLoadingSpinnerState(false);
@@ -110,9 +111,6 @@ class Loading extends BaseComponent {
 
     this.player.addEventListener(this.player.Event.ALL_ADS_COMPLETED, () => {
       this.props.updateLoadingSpinnerState(false);
-    });
-
-
     });
   }
 
