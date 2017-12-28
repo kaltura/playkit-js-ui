87d7ab7e59019ce1c873001b0576449206b42820
Dan Ziv <dan.ziv@kaltura.com>
2017-12-28 23:06:49 +0200

    Add fromJSON()

--- src/entities/ui-options.js
+++ src/entities/ui-options.js
@@ -14,19 +14,38 @@ export default class UIOptions {
     return this._targetId;
   }
 
+  set targetId(value: string): void {
+    if (typeof value !== 'string') return;
+    this._targetId = value;
+  }
+
   get logLevel(): string {
     return this._logLevel;
   }
 
   set logLevel(value: string): void {
+    if (typeof value !== 'string') return;
     if (LogLevel[value]) {
       this._logLevel = value;
     }
   }
 
   constructor(targetId: string, logLevel?: string) {
-    this._targetId = targetId;
-    this.logLevel = logLevel || 'ERROR';
+    if (typeof targetId === 'string') {
+      this.targetId = targetId;
+      this.logLevel = logLevel || 'ERROR';
+    } else if (typeof targetId === 'object') {
+      this.fromJSON(targetId);
+    }
+  }
+
+  fromJSON(json: UIOptionsObject): void {
+    if (json.targetId) {
+      this.targetId = json.targetId;
+    }
+    if (json.logLevel) {
+      this.logLevel = json.logLevel;
+    }
   }
 
   toJSON(): UIOptionsObject {
