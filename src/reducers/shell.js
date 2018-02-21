240e69a5c4b4adc2f848f7f313df0ca801fbf91c
Dan Ziv <dan.ziv@kaltura.com>
2018-02-21 17:43:33 +0200

    fix: dropdown position calculation (#181)
    Fix calculation of dropdown positioning to relative to player container.

--- src/reducers/shell.js
+++ src/reducers/shell.js
@@ -4,8 +4,7 @@ export const types = {
   REMOVE_PLAYER_CLASS: 'shell/REMOVE_PLAYER_CLASS',
   UPDATE_IS_MOBILE: 'shell/UPDATE_IS_MOBILE',
   UPDATE_PRE_PLAYBACK: 'shell/UPDATE_PRE_PLAYBACK',
-  UPDATE_PLAYER_WIDTH: 'shell/UPDATE_PLAYER_WIDTH',
-  UPDATE_PLAYER_HEIGHT: 'shell/UPDATE_PLAYER_HEIGHT',
+  UPDATE_PLAYER_CLIENT_RECT: 'shell/UPDATE_PLAYER_CLIENT_RECT',
   UPDATE_DOCUMENT_WIDTH: 'shell/UPDATE_DOCUMENT_WIDTH',
   UPDATE_PLAYER_HOVER_STATE: 'shell/UPDATE_PLAYER_HOVER_STATE',
   UPDATE_PLAYER_NAV_STATE: 'shell/UPDATE_PLAYER_NAV_STATE'
@@ -46,16 +45,10 @@ export default (state: Object = initialState, action: Object) => {
         prePlayback: action.prePlayback
       };
 
-    case types.UPDATE_PLAYER_HEIGHT:
+    case types.UPDATE_PLAYER_CLIENT_RECT:
       return {
         ...state,
-        playerHeight: action.playerHeight
-      };
-
-    case types.UPDATE_PLAYER_WIDTH:
-      return {
-        ...state,
-        playerWidth: action.playerWidth
+        playerClientRect: action.playerClientRect
       };
 
     case types.UPDATE_DOCUMENT_WIDTH:
@@ -86,8 +79,7 @@ export const actions = {
   removePlayerClass: (className: string) => ({type: types.REMOVE_PLAYER_CLASS, className}),
   updateIsMobile: (isMobile: boolean) => ({type: types.UPDATE_IS_MOBILE, isMobile}),
   updatePrePlayback: (prePlayback: boolean) => ({type: types.UPDATE_PRE_PLAYBACK, prePlayback}),
-  updatePlayerWidth: (playerWidth: number) => ({type: types.UPDATE_PLAYER_WIDTH, playerWidth}),
-  updatePlayerHeight: (playerHeight: number) => ({type: types.UPDATE_PLAYER_HEIGHT, playerHeight}),
+  updatePlayerClientRect: (playerClientRect: Object) => ({type: types.UPDATE_PLAYER_CLIENT_RECT, playerClientRect}),
   updateDocumentWidth: (documentWidth: number) => ({type: types.UPDATE_DOCUMENT_WIDTH, documentWidth}),
   updatePlayerHoverState: (hover: boolean) => ({type: types.UPDATE_PLAYER_HOVER_STATE, hover}),
   updatePlayerNavState: (nav: boolean) => ({type: types.UPDATE_PLAYER_NAV_STATE, nav})
