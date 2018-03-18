cd59593194cdf7406975a72797bd6a9f145bc3c5
OrenMe <oren.me@gmail.com>
2018-03-18 11:31:16 +0200

    expose preact to application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // @flow
 import UIManager from './ui-manager'
+import * as preact from 'preact';
 
 declare var __VERSION__: string;
 declare var __NAME__: string;
 
 export {h} from 'preact';
+export {preact};
 
 // ui presets
 export * as presets from './ui-presets';
