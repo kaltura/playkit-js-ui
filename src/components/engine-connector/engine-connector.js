d17997bd7814093f83edb4c78102eb441d6f77e4
Dan Ziv <dan.ziv@kaltura.com>
2018-11-05 10:02:27 +0200

    remove unused style

--- src/components/engine-connector/engine-connector.js
+++ src/components/engine-connector/engine-connector.js
@@ -1,6 +1,5 @@
 //@flow
 import {h} from 'preact';
-import style from '../../styles/style.scss';
 import {connect} from 'preact-redux';
 import {bindActions} from '../../utils/bind-actions';
 import {default as reduce, actions} from '../../reducers/engine';
