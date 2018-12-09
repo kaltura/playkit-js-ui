2ed6c355944e8ad088cbbf456e508333552e37b1
Yair Ansbacher <yair.ansbacher@kaltura.com>
2018-12-09 12:42:46 +0200

    fix(FEC-8710): watermark covers the playlist countdown (#305)
    * move the watermark component into the player-gui before the bottom bar component
    * use component's static `shouldRender` instead of the generic `shouldRenderComponent`

--- src/utils/component-config.js
+++ src/utils/component-config.js
@@ -28,15 +28,4 @@ function getComponentStateFromComponentConfig(component: string, oldState: Objec
   return oldState;
 }
 
-/**
- * Checks if component should be rendered based on its value in the store.
- * @param {Object} config - Config store
- * @param {string} alias - Component alias
- * @return {boolean} - Whether component should be rendered
- */
-function shouldRenderComponent(config: Object, alias: string) {
-  const componentConfig = config.components[alias];
-  return !(Object.keys(componentConfig).length === 0 && componentConfig.constructor === Object);
-}
-
-export {shouldRenderComponent, getComponentStateFromConfig, getComponentStateFromComponentConfig};
+export {getComponentStateFromConfig, getComponentStateFromComponentConfig};
