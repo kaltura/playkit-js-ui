273357f3186dc2e602961cf6f13ebf9f2bfee501
Dan Ziv <dan.ziv@kaltura.com>
2019-01-01 13:04:45 +0200

    finish logic

--- src/components/watchdog/watchdog.js
+++ src/components/watchdog/watchdog.js
@@ -1,17 +1,8 @@
 //@flow
 import style from '../../styles/style.scss';
 import {h} from 'preact';
-import {connect} from 'preact-redux';
 import BaseComponent from '../base';
 
-/**
- * mapping state to props
- * @param {*} state - redux store state
- * @returns {Object} - mapped state to this component
- */
-const mapStateToProps = state => ({});
-
-@connect(mapStateToProps)
 class Watchdog extends BaseComponent {
   watchdog: Array<Object>;
 
@@ -20,14 +11,26 @@ class Watchdog extends BaseComponent {
     this.watchdog = this.player.config.watchdog;
     this.watchdog.forEach(p => p.seen = false);
     this.player.addEventListener(this.player.Event.TIME_UPDATE, () => {
-      const point = this.watchdog.find(p => p.start <= this.player.currentTime && !p.seen);
-      if (point && !this.state.active) {
-        // this.setState({active: true, point: });
+      if (this.state.active) {
+        if (this.state.point.end <= this.player.currentTime) {
+          this.state.point.seen = true;
+          this.setState({active: false, point: null});
+        }
+      } else {
+        const point = this.watchdog.find(p => p.start <= this.player.currentTime && !p.seen);
+        if (point && !this.state.active) {
+          this.setState({active: true, point: point});
+        }
       }
     });
   }
 
   render(props: any): React$Element<any> {
+    if (this.state.active) {
+      console.warn('::: active', this.state.point);
+    } else {
+      console.warn('::: not active');
+    }
     return (
       <div className={style.watchdog}>
         <div className={style.familyModeText}>FAMILY MODE</div>
