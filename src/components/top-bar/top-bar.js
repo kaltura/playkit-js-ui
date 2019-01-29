258e4e46087834db413e9b90d6f811530d36b024
yair.ansbacher <yairans@gmail.com>
2019-01-29 17:29:54 +0200

    add top bar

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -1,6 +1,19 @@
 //@flow
 import style from '../../styles/style.scss';
 import {h, Component} from 'preact';
+import {connect} from 'preact-redux';
+
+/**
+ * mapping state to props
+ * @param {*} state - redux store state
+ * @returns {Object} - mapped state to this component
+ */
+const mapStateToProps = state => ({
+  isCasting: state.engine.isCasting,
+  isPlaybackEnded: state.engine.isPlaybackEnded
+});
+
+@connect(mapStateToProps)
 
 /**
  * TopBar component
@@ -18,6 +31,9 @@ class TopBar extends Component {
    * @memberof TopBar
    */
   render(props: any): React$Element<any> {
+    if (props.isCasting && props.isPlaybackEnded) {
+      return undefined;
+    }
     return <div className={style.topBar}>{props.children}</div>;
   }
 }
