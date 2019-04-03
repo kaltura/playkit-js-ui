033ad3cd489d5c529f44c4221dc67528e0da7c39
OrenMe <oren.me@gmail.com>
2019-04-03 09:29:04 +0300

    fix(FEC-9010): share button shows on preplay screen

--- src/ui-presets/live.js
+++ src/ui-presets/live.js
@@ -38,7 +38,8 @@ export function liveUI(props: any): React$Element<any> {
       <Loading player={props.player} />
       <div className={style.playerGui} id="player-gui">
         <OverlayPortal />
-        <UnmuteIndication />
+        <UnmuteIndication player={props.player} />
+        <ShareControl player={props.player} />
         <OverlayAction player={props.player} />
         <PlaybackControls player={props.player} />
         <BottomBar>
@@ -59,7 +60,6 @@ export function liveUI(props: any): React$Element<any> {
         </BottomBar>
       </div>
       {Watermark.shouldRender(props) ? <Watermark player={props.player} /> : undefined}
-      <ShareControl player={props.player} />
       <PrePlaybackPlayOverlay player={props.player} />
       <CastBeforePlay player={props.player} />
       <PictureInPictureOverlay player={props.player} />
--- src/ui-presets/playback.js
+++ src/ui-presets/playback.js
@@ -42,6 +42,7 @@ export function playbackUI(props: any): React$Element<any> {
       <div className={style.playerGui} id="player-gui">
         <OverlayPortal />
         <UnmuteIndication player={props.player} />
+        <ShareControl player={props.player} />
         <OverlayAction player={props.player} />
         <PlaybackControls player={props.player} />
         {PlaylistNextScreen.shouldRender(props) ? <PlaylistNextScreen player={props.player} /> : undefined}
@@ -64,7 +65,6 @@ export function playbackUI(props: any): React$Element<any> {
         </BottomBar>
       </div>
       {Watermark.shouldRender(props) ? <Watermark player={props.player} /> : undefined}
-      <ShareControl player={props.player} />
       {PlaylistCountdown.shouldRender(props) ? <PlaylistCountdown player={props.player} /> : undefined}
       <PrePlaybackPlayOverlay player={props.player} />
       <CastBeforePlay player={props.player} />
