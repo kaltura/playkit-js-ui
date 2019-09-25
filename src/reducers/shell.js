c91427efa505537dfbae154175039da4ca9c7100
Eran Sakal <eran@sakalim.com>
2019-09-25 10:26:00 +0300

    add side panel to reducer

--- src/reducers/shell.js
+++ src/reducers/shell.js
@@ -5,20 +5,55 @@ export const types = {
   UPDATE_IS_MOBILE: 'shell/UPDATE_IS_MOBILE',
   UPDATE_PLAYER_SIZE: 'shell/UPDATE_PLAYER_SIZE',
   UPDATE_PLAYER_CLIENT_RECT: 'shell/UPDATE_PLAYER_CLIENT_RECT',
+  UPDATE_PLAYER_WRAPPER_CLIENT_RECT: 'shell/UPDATE_PLAYER_WRAPPER_CLIENT_RECT',
   UPDATE_DOCUMENT_WIDTH: 'shell/UPDATE_DOCUMENT_WIDTH',
   UPDATE_PLAYER_HOVER_STATE: 'shell/UPDATE_PLAYER_HOVER_STATE',
   UPDATE_PLAYER_NAV_STATE: 'shell/UPDATE_PLAYER_NAV_STATE',
   UPDATE_BOTTOM_BAR_HOVER_ACTIVE: 'shell/UPDATE_BOTTOM_BAR_HOVER_ACTIVE',
   UPDATE_SMART_CONTAINER_OPEN: 'shell/UPDATE_SMART_CONTAINER_OPEN',
-  UPDATE_ACTIVE_PRESET_NAME: 'shell/UPDATE_ACTIVE_PRESET_NAME'
+  UPDATE_ACTIVE_PRESET_NAME: 'shell/UPDATE_ACTIVE_PRESET_NAME',
+  UPDATE_SIDE_PANEL_MODE: 'shell/UPDATE_SIDE_PANEL_MODE',
+  UPDATE_SIDE_PANEL_SIZE: 'shell/UPDATE_SIDE_PANEL_SIZE',
+  UPDATE_SIDE_PANELS_ALLOWED: 'shell/UPDATE_SIDE_PANELS_ALLOWED'
+};
+
+export const SidePanelOrientation = {
+  VERTICAL: 'VERTICAL',
+  HORIZONTAL: 'HORIZONTAL'
+};
+
+export const SidePanelPositions = {
+  LEFT: 'LEFT',
+  TOP: 'TOP',
+  BOTTOM: 'BOTTOM',
+  RIGHT: 'RIGHT'
+};
+
+export const SidePanelModes = {
+  ALONG_SIDE_THE_VIDEO: 'ALONG_SIDE_THE_VIDEO',
+  HIDDEN: 'HIDDEN',
+  OVER_THE_VIDEO: 'OVER_THE_VIDEO'
 };
 
 export const initialState = {
   playerClasses: [],
+  playerClientRect: {width: 0, height: 0}, // todo sakal oren decide if this is relevant
+  playerWrapperClientRect: {width: 0, height: 0},
   playerHover: false,
   playerNav: false,
   smartContainerOpen: false,
-  activePresetName: ''
+  activePresetName: '',
+  sidePanelsModes: {
+    [SidePanelPositions.LEFT]: SidePanelModes.HIDDEN,
+    [SidePanelPositions.RIGHT]: SidePanelModes.HIDDEN,
+    [SidePanelPositions.TOP]: SidePanelModes.HIDDEN,
+    [SidePanelPositions.BOTTOM]: SidePanelModes.HIDDEN
+  },
+  sidePanelsSizes: {
+    [SidePanelOrientation.VERTICAL]: {min: 240, max: 480, ratio: 0.33},
+    [SidePanelOrientation.HORIZONTAL]: {min: 144, max: 288, ratio: 0.33}
+  },
+  sidePanelsAllowed: false
 };
 
 export default (state: Object = initialState, action: Object) => {
@@ -52,6 +87,12 @@ export default (state: Object = initialState, action: Object) => {
       return {
         ...state,
         playerClientRect: action.playerClientRect
+      };
+
+    case types.UPDATE_PLAYER_WRAPPER_CLIENT_RECT:
+      return {
+        ...state,
+        playerWrapperClientRect: action.playerWrapperClientRect
       };
 
     case types.UPDATE_DOCUMENT_WIDTH:
@@ -90,6 +131,38 @@ export default (state: Object = initialState, action: Object) => {
         activePresetName: action.activePresetName
       };
 
+    case types.UPDATE_SIDE_PANEL_MODE:
+      return {
+        ...state,
+        sidePanelsModes: {
+          ...state.sidePanelsModes,
+          [action.position]: action.sidePanelMode
+        }
+      };
+
+    case types.UPDATE_SIDE_PANEL_SIZE: {
+      const {ratio, min, max} = action.options;
+      const prevValues = state.sidePanelsSizes[action.orientation];
+      const newSizes = {
+        ratio: typeof ratio === 'number' && ratio <= 1 ? ratio : prevValues.ratio,
+        min: typeof min === 'number' ? min : prevValues.min,
+        max: typeof max === 'number' ? max : prevValues.max
+      };
+      return {
+        ...state,
+        sidePanelsSizes: {
+          ...state.sidePanelsSizes,
+          [action.orientation]: newSizes
+        }
+      };
+    }
+
+    case types.UPDATE_SIDE_PANELS_ALLOWED:
+      return {
+        ...state,
+        sidePanelsAllowed: action.allowed
+      };
+
     default:
       return state;
   }
@@ -101,10 +174,22 @@ export const actions = {
   updateIsMobile: (isMobile: boolean) => ({type: types.UPDATE_IS_MOBILE, isMobile}),
   updatePlayerSize: (playerSize: string) => ({type: types.UPDATE_PLAYER_SIZE, playerSize}),
   updatePlayerClientRect: (playerClientRect: Object) => ({type: types.UPDATE_PLAYER_CLIENT_RECT, playerClientRect}),
+  updatePlayerWrapperClientRect: (playerWrapperClientRect: Object) => ({type: types.UPDATE_PLAYER_WRAPPER_CLIENT_RECT, playerWrapperClientRect}),
   updateDocumentWidth: (documentWidth: number) => ({type: types.UPDATE_DOCUMENT_WIDTH, documentWidth}),
   updatePlayerHoverState: (hover: boolean) => ({type: types.UPDATE_PLAYER_HOVER_STATE, hover}),
   updatePlayerNavState: (nav: boolean) => ({type: types.UPDATE_PLAYER_NAV_STATE, nav}),
   updateBottomBarHoverActive: (active: boolean) => ({type: types.UPDATE_BOTTOM_BAR_HOVER_ACTIVE, active}),
   updateSmartContainerOpen: (open: boolean) => ({type: types.UPDATE_SMART_CONTAINER_OPEN, open}),
-  updateActivePresetName: (activePresetName: string) => ({type: types.UPDATE_ACTIVE_PRESET_NAME, activePresetName})
+  updateActivePresetName: (activePresetName: string) => ({type: types.UPDATE_ACTIVE_PRESET_NAME, activePresetName}),
+  updateSidePanelMode: (position: SidePanelPositions, sidePanelMode: SidePanelModes) => ({
+    type: types.UPDATE_SIDE_PANEL_MODE,
+    position,
+    sidePanelMode
+  }),
+  updateSidePanelSize: (orientation: SidePanelOrientation, options) => ({
+    type: types.UPDATE_SIDE_PANEL_SIZE,
+    orientation,
+    options
+  }),
+  updateSidePanelsAllowed: (allowed: boolean) => ({type: types.UPDATE_SIDE_PANELS_ALLOWED, allowed})
 };
