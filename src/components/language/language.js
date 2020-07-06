e74421a9fe39019824269297bf7d58abc2a4a58c
Roy Bregman <roy.bregman@kaltura.com>
2020-07-06 10:35:40 +0300

    fix(FEC-10226): in a small player in the captions menu ,menu doesn't close with keyboard navigation

--- src/components/language/language.js
+++ src/components/language/language.js
@@ -147,7 +147,7 @@ class Language extends Component {
    * @returns {void}
    * @memberof Language
    */
-  onControlButtonClick(): void {
+  toggleSmartContainerOpen(): void {
     this.setState(prevState => {
       return {smartContainerOpen: !prevState.smartContainerOpen};
     });
@@ -213,7 +213,7 @@ class Language extends Component {
             aria-haspopup="true"
             aria-label={this.props.buttonLabel}
             className={this.state.smartContainerOpen ? [style.controlButton, style.active].join(' ') : style.controlButton}
-            onClick={() => this.onControlButtonClick()}>
+            onClick={() => this.toggleSmartContainerOpen()}>
             <Icon type={IconType.Language} />
           </Button>
         </Tooltip>
@@ -223,7 +223,7 @@ class Language extends Component {
           <SmartContainer
             targetId={this.props.player.config.targetId}
             title={<Text id="language.title" />}
-            onClose={() => this.onControlButtonClick()}>
+            onClose={() => this.toggleSmartContainerOpen()}>
             {audioOptions.length <= 1 ? (
               undefined
             ) : (
@@ -250,7 +250,7 @@ class Language extends Component {
               <AdvancedCaptionsAnchor
                 isPortal={this.props.isMobile || this.props.isSmallSize}
                 onMenuChosen={() => this.toggleCVAAOverlay()}
-                onClose={() => this.onControlButtonClick()}
+                onClose={() => this.toggleSmartContainerOpen()}
               />
             )}
           </SmartContainer>
@@ -260,7 +260,7 @@ class Language extends Component {
             <CVAAOverlay
               onClose={() => {
                 this.toggleCVAAOverlay();
-                this.onControlButtonClick();
+                this.toggleSmartContainerOpen();
               }}
             />,
             document.querySelector(portalSelector)
