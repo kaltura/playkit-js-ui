c294b4061067a1bfd2d9e67cf792fc53711f408d
yair.ansbacher <yairans@gmail.com>
2021-05-07 10:21:25 +0300

    add share translations

--- src/components/share/default-config.js
+++ src/components/share/default-config.js
@@ -2,24 +2,24 @@ const defaultConfig = [
   {
     name: 'facebook',
     templateUrl: 'https://www.facebook.com/sharer/sharer.php?u={shareUrl}',
-    title: 'share.facebook',
-    ariaLabel: 'share.facebook',
+    title: 'share.share-on-facebook',
+    ariaLabel: 'share.share-on-facebook',
     iconType: 'default',
     svg: ''
   },
   {
     name: 'linkedin',
     templateUrl: 'https://www.linkedin.com/shareArticle?mini=true&url={shareUrl}',
-    title: 'share.linkedin',
-    ariaLabel: 'share.linkedin',
+    title: 'share.share-on-linkedin',
+    ariaLabel: 'share.share-on-linkedin',
     iconType: 'default',
     svg: ''
   },
   {
     name: 'twitter',
     templateUrl: 'https://twitter.com/share?url={shareUrl}',
-    title: 'share.twitter',
-    ariaLabel: 'share.twitter',
+    title: 'share.share-on-twitter',
+    ariaLabel: 'share.share-on-twitter',
     iconType: 'default',
     svg: ''
   }
