bfaf4312920b35e150302be34c5b00aa14b3a079
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2021-07-08 14:26:47 +0300

    fix(FEC-11292): when in full screen, button tool tip should state "Exit Full Screen" (#619)
    Issue: When video in full screen, button tooltip state "Full Screen" instead of "Exit Full Screen".
    Solution: add tooltip state "Exit Full Screen" when on fullscreen mode .

--- src/components/fullscreen/fullscreen.js
+++ src/components/fullscreen/fullscreen.js
@@ -19,7 +19,7 @@ import {ButtonControl} from 'components/button-control';
  * @returns {Object} - mapped state to this component
  */
 const mapStateToProps = state => ({
-  fullscreen: state.engine.fullscreen,
+  isInFullscreen: state.engine.fullscreen,
   targetId: state.config.targetId
 });
 
@@ -37,7 +37,10 @@ const COMPONENT_NAME = 'Fullscreen';
 @withLogger(COMPONENT_NAME)
 @withKeyboardEvent(COMPONENT_NAME)
 @withEventDispatcher(COMPONENT_NAME)
-@withText({fullscreenText: 'controls.fullscreen'})
+@withText({
+  fullscreenText: 'controls.fullscreen',
+  fullscreenExitText: 'controls.fullscreenExit'
+})
 class Fullscreen extends Component {
   _keyboardEventHandlers: Array<KeyboardEventHandlers> = [
     {
@@ -116,11 +119,11 @@ class Fullscreen extends Component {
   render(): React$Element<any> {
     return (
       <ButtonControl name={COMPONENT_NAME}>
-        <Tooltip label={this.props.fullscreenText}>
+        <Tooltip label={this.props.isInFullscreen ? this.props.fullscreenExitText : this.props.fullscreenText}>
           <Button
             tabIndex="0"
-            aria-label={this.props.fullscreenText}
-            className={this.props.fullscreen ? [style.controlButton, style.isFullscreen].join(' ') : style.controlButton}
+            aria-label={this.props.isInFullscreen ? this.props.fullscreenExitText : this.props.fullscreenText}
+            className={this.props.isInFullscreen ? [style.controlButton, style.isFullscreen].join(' ') : style.controlButton}
             onClick={this.toggleFullscreen}>
             <Icon type={IconType.Maximize} />
             <Icon type={IconType.Minimize} />
