06572b7a5c95ac782a2cef9ce6d5473c62bc959a
JonathanTGold <jonathan.gold@86@gmail.com>
2021-09-30 03:10:15 +0200

    remove on close event props

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -129,6 +129,7 @@ class DropDown extends Component {
    * @memberof Menu
    */
   handleClickOutside = (e: Event) => {
+    console.log(e.timeStamp, this.dropDownId);
     if (this.dropDownId !== e.timeStamp) {
       if (!this.props.isMobile && !this.props.isSmallSize) {
         this.onClose();
@@ -203,7 +204,7 @@ class DropDown extends Component {
           </span>
           <Icon type={IconType.ArrowDown} />
           {!this.state.dropMenuActive ? undefined : (
-            <Menu parentEl={this._el} options={props.options} onMenuChosen={this.onMenuChosen} onClose={this.onClose} />
+            <Menu parentEl={this._el} options={props.options} onMenuChosen={this.onMenuChosen} />
           )}
         </div>
       </div>
