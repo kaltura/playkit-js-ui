499376fd917ed16aaa4bfa0f3febf2f60d80c8c0
JonathanTGold <jonathan.gold@86@gmail.com>
2023-11-13 15:40:56 +0200

    upgrade webpack deps and the coorsponding conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,115 @@
-'use strict';
-
 const webpack = require('webpack');
 const path = require('path');
 const packageData = require('./package.json');
+const TerserPlugin = require('terser-webpack-plugin');
 const CSS_MODULE_PREFIX = 'playkit';
 
-let plugins = [
-  new webpack.DefinePlugin({
-    __VERSION__: JSON.stringify(packageData.version),
-    __NAME__: JSON.stringify(packageData.name),
-    __CSS_MODULE_PREFIX__: JSON.stringify(CSS_MODULE_PREFIX),
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-  })
-];
-
-module.exports = {
-  context: __dirname + '/src',
-  entry: {
-    'playkit-ui': 'index.js'
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
-    library: ['playkit', 'ui'],
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    devtoolModuleFilenameTemplate: './ui/[resource-path]'
-  },
-  devtool: 'source-map',
-  plugins,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: ['babel-loader', 'eslint-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {attributes: {id: `${packageData.name}`}}
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              localsConvention: 'camelCase',
-              modules: {
-                localIdentName: `${CSS_MODULE_PREFIX}-[local]`
-              }
+module.exports = (env, {mode}) => {
+  return {
+    entry: './src/index.js',
+    optimization: {
+      minimize: mode !== 'development',
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+          terserOptions: {
+            format: {
+              comments: false
             }
-          },
-          {
-            loader: 'sass-loader'
           }
-        ]
+        })
+      ]
+    },
+    // devtool: 'source-map',
+    devtool: 'eval-source-map',
+    module: {
+      rules: [
+        // {
+        //   // test: /\.(ts|js)$/,
+        //   test: /\.ts$/,
+        //   exclude: /node_modules/,
+        //   use: {
+        //     loader: 'babel-loader',
+        //     options: {
+        //       presets: [['@babel/preset-env', {
+        //         loose: true,
+        //         bugfixes: true,
+        //         "targets": {
+        //           "browsers": ["chrome >= 47", "firefox >= 51", "ie >= 11", "safari >= 8", "ios >= 8", "android >= 4"]
+        //         }
+        //       }], '@babel/preset-typescript'],
+        //       plugins: [['@babel/plugin-transform-runtime']]
+        //     }
+        //   }
+        // },
+        {
+          test: /\.js$/,
+          use: ['babel-loader', 'eslint-loader'],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader',
+              options: {attributes: {id: `${packageData.name}`}}
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                localsConvention: 'camelCase',
+                modules: {
+                  localIdentName: `${CSS_MODULE_PREFIX}-[local]`
+                }
+              }
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.scss', 'css'],
+      alias: {
+        components: path.resolve(__dirname, 'src/components/'),
+        reducers: path.resolve(__dirname, 'src/reducers/'),
+        utils: path.resolve(__dirname, 'src/utils/'),
+        event: path.resolve(__dirname, 'src/event'),
+        react: 'preact/compat',
+        'react-dom': 'preact/compat'
       }
+    },
+    externals: {
+      '@playkit-js/kaltura-player-js': ['KalturaPlayer']
+    },
+    output: {
+      filename: 'playkit-ui.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        umdNamedDefine: true,
+        name: ['playkit', 'ui'],
+        type: 'umd'
+        // devtoolModuleFilenameTemplate: './ui/[resource-path]'
+      },
+      clean: true
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'demo')
+      },
+      client: {
+        progress: true
+      }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VERSION__: JSON.stringify(packageData.version),
+        __NAME__: JSON.stringify(packageData.name),
+        __CSS_MODULE_PREFIX__: JSON.stringify(CSS_MODULE_PREFIX),
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      })
     ]
-  },
-  devServer: {
-    contentBase: __dirname + '/src'
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    alias: {
-      components: path.resolve(__dirname, 'src/components/'),
-      reducers: path.resolve(__dirname, 'src/reducers/'),
-      utils: path.resolve(__dirname, 'src/utils/'),
-      event: path.resolve(__dirname, 'src/event'),
-      react: 'preact/compat',
-      'react-dom': 'preact/compat'
-    }
-  },
-  externals: {
-    '@playkit-js/kaltura-player-js': {
-      commonjs: '@playkit-js/kaltura-player-js',
-      commonjs2: '@playkit-js/kaltura-player-js',
-      amd: '@playkit-js/kaltura-player-js',
-      root: ['KalturaPlayer']
-    }
-  }
+  };
 };
