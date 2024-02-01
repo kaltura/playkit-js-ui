7761f7ef49e1a49175a23639a6b38d3d20fa5218
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-02-01 19:49:01 +0200

    extract reused code to a function

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -40,7 +40,7 @@ class Logo extends Component<any, any> {
    */
   constructor(props: any) {
     super(props);
-    this.setState({isUrlClickable: true, urlLink: props.config.url});
+    this.setState({isUrlClickable: typeof props.config.url === 'string', urlLink: props.config.url});
   }
 
   /**
@@ -64,22 +64,31 @@ class Logo extends Component<any, any> {
     const url = this.props.config.url;
     if (url && url.indexOf(ENTRY_VAR) !== -1) {
       const {player, eventManager} = this.props;
-      const entryId = player.sources.id;
-      if (typeof entryId === 'string') {
-        this.setState({urlLink: url.replace(ENTRY_VAR, entryId)});
-      } else {
-        this.setState({isUrlClickable: false});
+      if (!this._maybeSetLogoUrlWithEntryId(url)) {
         eventManager.listen(player, player.Event.MEDIA_LOADED, () => {
-          const entryId = player.sources.id;
-          if (typeof entryId === 'string') {
-            this.setState({urlLink: url.replace(ENTRY_VAR, entryId), isUrlClickable: true});
-          } else {
-            this.props.logger.debug(`Logo url was not replaced; entry id was not found.`);
-            this.setState({isUrlClickable: false});
-          }
+          this._maybeSetLogoUrlWithEntryId(url);
         });
       }
     }
+  }
+
+  /**
+   * sets the url with the entry id
+   * @param {string} url - the url configured on the logo
+   * @returns {boolean} - whether the url was set with entry id
+   * @memberof Logo
+   */
+  private _maybeSetLogoUrlWithEntryId(url: string): boolean {
+    const {player} = this.props;
+    const entryId = player.sources.id;
+    if (typeof entryId === 'string') {
+      this.setState({urlLink: url.replace(ENTRY_VAR, entryId), isUrlClickable: true});
+      return true;
+    } else {
+      this.props.logger.debug(`Logo url was not replaced; entry id was not found.`);
+      this.setState({isUrlClickable: false});
+    }
+    return false;
   }
 
   /**
