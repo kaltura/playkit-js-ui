8d600427f2adf36ff039614bae353c2ec581cdf1
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-02-06 14:26:02 +0200

    feat(FEC-13630): expose tone ramp in user theme

--- src/types/user-theme.ts
+++ src/types/user-theme.ts
@@ -5,7 +5,20 @@ export type ColorType = {
   danger: string;
   warning: string;
   live: string;
+  // surface colors
   playerBackground: string;
+  paperSurface: string;
+  elevatedSurface: string;
+  protectionSurface: string;
+  // tone ramp colors
+  tone1: string;
+  tone2: string;
+  tone3: string;
+  tone4: string;
+  tone5: string;
+  tone6: string;
+  tone7: string;
+  tone8: string;
 };
 
 export type UserTheme = {
