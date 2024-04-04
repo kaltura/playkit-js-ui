52912d4d84fa2b76dab5aea18c3f9359e8c77645
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-04-04 18:20:00 +0300

    feat(FEC-13652): add meaningful error message to error overlay (#873)
    ### Description of the Changes
    
    Adding meaningful information on errors.
    
    - add new state to redux which contains `errorDetails`
      - category
      - title
      - message
    - add text values of (only) the core errors in `en.i18n.json` file, per
    requirements
    - add switch-case function which provides the title and message text
    keys, by the error category
    - align some styles according to design
    
    #### Resolves FEC-13652

---
+++ src/components/error-overlay/error-message-provider.ts
@@ -0,0 +1,27 @@
+type ErrorDetails = {
+  title: string;
+  message: string;
+};
+
+const errorsMap: Map<number, ErrorDetails> = new Map<number, ErrorDetails>([
+  [1, {title: 'network_error_title', message: 'network_error_message'}],
+  [2, {title: 'text_error_title', message: 'text_error_message'}],
+  [3, {title: 'media_error_title', message: 'media_error_message'}],
+  [4, {title: 'manifest_error_title', message: 'manifest_error_message'}],
+  [5, {title: 'streaming_error_title', message: 'streaming_error_message'}],
+  [6, {title: 'media_unavailable_error_title', message: 'drm_error_message'}],
+  [7, {title: 'default_error_title', message: 'default_error_message'}],
+  [9, {title: 'default_error_title', message: 'default_error_message'}],
+  [12, {title: 'media_not_ready_error_title', message: 'media_not_ready_error_message'}],
+  [13, {title: 'geo_location_error_title', message: 'geo_location_error_message'}],
+  [14, {title: 'media_unavailable_error_title', message: 'media_unavailable_error_message'}]
+]);
+
+const defaultError: ErrorDetails = {
+  title: 'default_error_title',
+  message: 'default_error_message'
+};
+
+export const getErrorDetailsByCategory = (errorCategory: number): ErrorDetails => {
+  return errorsMap.get(errorCategory) || defaultError;
+};
