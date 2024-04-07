54f7a917531cd5176900a597a4b73b621a78c01f
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-04-07 13:05:13 +0300

    feat(FEC-13652): add ADS category to core (#874)
    ### Description of the Changes
    
    since `ADS` error category has generic title and message error and the
    generic texts already exist in core ui, adding the category of `ADS` to
    ui.
    
    in addition, adding before each error item in the map a note of its
    category, so it will be clear to whoever is looking at it.
    
    #### Resolves FEC-13652

--- src/components/error-overlay/error-message-provider.ts
+++ src/components/error-overlay/error-message-provider.ts
@@ -4,16 +4,29 @@ type ErrorDetails = {
 };
 
 const errorsMap: Map<number, ErrorDetails> = new Map<number, ErrorDetails>([
+  /** NETWORK */
   [1, {title: 'network_error_title', message: 'network_error_message'}],
+  /** TEXT */
   [2, {title: 'text_error_title', message: 'text_error_message'}],
+  /** MEDIA */
   [3, {title: 'media_error_title', message: 'media_error_message'}],
+  /** MANIFEST */
   [4, {title: 'manifest_error_title', message: 'manifest_error_message'}],
+  /** STREAMING */
   [5, {title: 'streaming_error_title', message: 'streaming_error_message'}],
+  /** DRM */
   [6, {title: 'media_unavailable_error_title', message: 'drm_error_message'}],
+  /** PLAYER */
   [7, {title: 'default_error_title', message: 'default_error_message'}],
+  /** ADS */
+  [8, {title: 'default_error_title', message: 'default_error_message'}],
+  /** STORAGE */
   [9, {title: 'default_error_title', message: 'default_error_message'}],
+  /** MEDIA NOT READY */
   [12, {title: 'media_not_ready_error_title', message: 'media_not_ready_error_message'}],
+  /** GEOLOCATION */
   [13, {title: 'geo_location_error_title', message: 'geo_location_error_message'}],
+  /** KS RESTRICTION */
   [14, {title: 'media_unavailable_error_title', message: 'media_unavailable_error_message'}]
 ]);
 
