24968c426056fc2c79ea09e4b29bfc34dfbe78ef
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-05-08 14:53:09 +0300

    fix(FEC-13870): in and out to float causes size alignment issue (#881)
    ### Description of the Changes
    
    **- bugfix**
    
    **Issue:**
    when entering to floating mode and exiting floating mode very quick (ui
    has not finished to update in the floating mode), the layout in the
    original player is sometimes off.
    when the player size transitions from large to small, and before the
    player finishes to re-render the ui according to the new breakpoint,
    when transitioning back from small to large the ui still thinks it is in
    small size and is not being re-rendered.
    
    **Fix:**
    add a middleware for shell reducer, to manage the multiple `isSmallSize`
    updates, in order to prevent multiple and sometimes unnecessary updates
    and re-rendering.
    
    **Related PR-** https://github.com/kaltura/playkit-js-visibility/pull/72
    
    #### Resolves FEC-13870

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,7 @@ import {applyMiddleware} from 'redux';
 import {eventDispatcherMiddleware} from './event-dispatcher';
 import {loggerMiddleware} from './logger';
 import {UIOptionsObject} from '../types';
+import {debounceMiddleware} from './debounce-middleware';
 
 /**
  * Creates the redux middleware.
@@ -10,6 +11,6 @@ import {UIOptionsObject} from '../types';
  * @return {GenericStoreEnhancer} - The redux middleware.
  */
 const middleware = (player: any, config: UIOptionsObject): any =>
-  applyMiddleware<any, any>(loggerMiddleware(config), eventDispatcherMiddleware(player));
+  applyMiddleware<any, any>(loggerMiddleware(config), eventDispatcherMiddleware(player), debounceMiddleware());
 
 export {middleware};
