50ffe81e47a8b19768e72a80ef30fb8dfa9e1c2a
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-07-10 12:30:11 +0300

    remove unnecessary done() and use arrow function

--- tests/e2e/bottom-bar-registry-manager.spec.js
+++ tests/e2e/bottom-bar-registry-manager.spec.js
@@ -1,7 +1,7 @@
 import '../../src/index';
 import {BottomBarRegistryManager} from '../../src/components/bottom-bar';
 
-describe('BottomBarRegistryManager', function () {
+describe('BottomBarRegistryManager', () => {
   const myComponentName = 'MyComponent';
   const iconDto = {
     ariaLabel: 'aria label',
@@ -15,39 +15,36 @@ describe('BottomBarRegistryManager', function () {
 
   let bottomBarRegistryManager;
 
-  beforeEach(function () {
+  beforeEach(() => {
     bottomBarRegistryManager = new BottomBarRegistryManager();
   });
 
-  afterEach(function () {
+  afterEach(() => {
     bottomBarRegistryManager = null;
   });
 
-  describe('register API', function () {
-    it('Should register a component to the manager', function (done) {
+  describe('register API', () => {
+    it('Should register a component to the manager', () => {
       bottomBarRegistryManager.register(myComponentName, iconDto);
       bottomBarRegistryManager.registry.size.should.equal(1);
-      done();
     });
   });
 
-  describe('unregister API', function () {
-    it('Should unregister a component from the manager', function (done) {
+  describe('unregister API', () => {
+    it('Should unregister a component from the manager', () => {
       bottomBarRegistryManager.register(myComponentName, iconDto);
       bottomBarRegistryManager.registry.size.should.equal(1);
       bottomBarRegistryManager.unregister(myComponentName);
       bottomBarRegistryManager.registry.size.should.equal(0);
-      done();
     });
   });
 
-  describe('clear API', function () {
-    it('Should clear the registry of the manager', function (done) {
+  describe('clear API', () => {
+    it('Should clear the registry of the manager', () => {
       bottomBarRegistryManager.register(myComponentName, iconDto);
       bottomBarRegistryManager.registry.size.should.equal(1);
       bottomBarRegistryManager.clear();
       bottomBarRegistryManager.registry.size.should.equal(0);
-      done();
     });
   });
 });
