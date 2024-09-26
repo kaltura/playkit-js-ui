c99de0da98ee6ea253e001b8b7a13eb3308cc7bc
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-09-26 11:13:52 +0300

    fix lint

--- src/components/overlay-portal/overlay-portal-utils.ts
+++ src/components/overlay-portal/overlay-portal-utils.ts
@@ -5,4 +5,4 @@ const OVERLAY_PORTAL_SELECTOR = '.overlay-portal';
 export const getOverlayPortalElement = (player: KalturaPlayer): Element | null => {
   const targetOverlayEl: HTMLDivElement | Document = (document.getElementById(player.config.targetId) as HTMLDivElement) || document;
   return targetOverlayEl.querySelector(OVERLAY_PORTAL_SELECTOR);
-}
+};
