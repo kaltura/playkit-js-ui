3f40e26811518971382af4f0bfba1c2d699b8e63
Serhii <serhii.marchenko@Kaltura.com>
2025-02-15 16:34:41 +0200

    fix(FEC-14377): AAD component fix (#983)
    #### Resolves https://kaltura.atlassian.net/browse/FEC-14377

--- src/components/advanced-audio-desc/advanced-audio-desc.tsx
+++ src/components/advanced-audio-desc/advanced-audio-desc.tsx
@@ -45,6 +45,10 @@ class AdvancedAudioDesc extends Component<any, any> implements IconComponent {
     registerToBottomBar(COMPONENT_NAME, props.player, () => this.registerComponent());
   }
 
+  get advancedAudioDesc(): boolean {
+    return redux.useStore().getState().settings.advancedAudioDesc;
+  }
+
   public registerComponent(): any {
     return {
       ariaLabel: () => this.getComponentText(),
@@ -60,7 +64,7 @@ class AdvancedAudioDesc extends Component<any, any> implements IconComponent {
   }
 
   public getComponentText = (): any => {
-    return this.props.advancedAudioDescEnabled ? this.props.advancedAudioDescEnabledText : this.props.advancedAudioDescDisabledText;
+    return this.advancedAudioDesc ? this.props.advancedAudioDescEnabledText : this.props.advancedAudioDescDisabledText;
   };
 
   public getSvgIcon = (): any => {
@@ -83,7 +87,7 @@ class AdvancedAudioDesc extends Component<any, any> implements IconComponent {
    * @memberof AdvancedAudioDesc
    */
   private onClick = (): void => {
-    const checked = !this.props.advancedAudioDescEnabled;
+    const checked = !this.advancedAudioDesc;
     this.props.updateAdvancedAudioDesc(checked);
     this.props.notifyClick({type: 'AdvancedAudioDescription', checked});
   };
@@ -113,7 +117,13 @@ class AdvancedAudioDesc extends Component<any, any> implements IconComponent {
     return !this._shouldRender() ? undefined : (
       <ButtonControl name={COMPONENT_NAME} className={[style.noIdleControl, this.props.classNames ? this.props.classNames.join(' ') : ''].join(' ')}>
         <Tooltip label={this.getComponentText()}>
-          <Button tabIndex="0" aria-label={this.getComponentText()} className={`${style.controlButton}`} ref={innerRef} onClick={this.onClick} onKeyDown={this.onKeyDown}>
+          <Button
+            tabIndex="0"
+            aria-label={this.getComponentText()}
+            className={`${style.controlButton}`}
+            ref={innerRef}
+            onClick={this.onClick}
+            onKeyDown={this.onKeyDown}>
             <Icon type={this.props.advancedAudioDescEnabled ? IconType.AdvancedAudioDescriptionActive : IconType.AdvancedAudioDescription} />
           </Button>
         </Tooltip>
