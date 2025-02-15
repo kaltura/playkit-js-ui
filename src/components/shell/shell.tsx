5d85cedc33d4fa66f3ab1d91b4b7d0dcfaee2042
yambenari24 <yam.benari@kaltura.com>
2025-02-15 11:47:11 +0200

    fix(SUP-46391): Player Live Full Screen Bug when browser window is not maximized (#976)
    Issue: 
    When browser width is small, side pannel is on, and enter full screen,
    there is a gap between side pannel and player.
    
    Fix:
    Fix timing issue : the issue related to the timing of the DOM updates.
    When resizing the window or triggering an actions that affect the
    layout, the client rect of the player container getBoundingClientRect()
    is not updated immediately after the resize. This causes discrepancies
    between the actual layout and the values being used in the state.
    
    solved [SUP-46391](https://kaltura.atlassian.net/browse/SUP-46391)
    
    [SUP-46391]:
    https://kaltura.atlassian.net/browse/SUP-46391?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/components/shell/shell.tsx
+++ src/components/shell/shell.tsx
@@ -262,6 +262,9 @@ class Shell extends Component<any, any> {
     if (playerContainer) {
       this.props.updatePlayerClientRect(playerContainer.getBoundingClientRect());
     }
+    setTimeout(() => {
+      this.props.updatePlayerClientRect(playerContainer.getBoundingClientRect());
+    }, ON_PLAYER_RECT_CHANGE_DEBOUNCE_DELAY);
   };
 
   /**
