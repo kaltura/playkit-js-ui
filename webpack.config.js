f7edc012b3e8f8b61dcaa106ee7e86bb4a625f9e
lianbenjamin <lian.binyamin@kaltura.com>
2025-03-02 16:12:21 +0200

    use webpack common dependency

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const packageData = require('./package.json');
 const CSS_MODULE_PREFIX = 'playkit';
+const {insertStylesWithNonce} = require('@playkit-js/webpack-common');
 
 module.exports = (env, {mode}) => {
   return {
@@ -42,12 +43,7 @@ module.exports = (env, {mode}) => {
                 attributes: {
                   id: `${packageData.name}`
                 },
-                insert: function insertStylesWithNonce(styleElement) {
-                  if (typeof window.kalturaGlobalConfig?.stylesNonce === 'string') {
-                    styleElement.setAttribute('nonce', window.kalturaGlobalConfig.stylesNonce);
-                  }
-                  document.head.appendChild(styleElement);
-                }
+                insert: insertStylesWithNonce
               }
             },
             {
