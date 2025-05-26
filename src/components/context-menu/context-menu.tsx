c590982ee076818ef317472de21759b9319e49d0
Sivan Agranov <88330203+SivanA-Kaltura@users.noreply.github.com>
2025-05-26 17:08:45 +0300

    fix(FEC-14497): Add Debug Info Option to the Player | Long pressing on ios mobile doesnt show "Copy debug info" button".
    ### Description of the Changes
    
    Handle debug info context menu in iOS:
    
    - use touch events instead of contextmenu event, which doesnt fire in
    iOS
    - protect against navigator.clipboard.writeText not working in iOS under
    certain conditions (for example, it works in some iframe pages but
    doesn't work in others)
    
    Resolves FEC-14497

--- src/components/context-menu/context-menu.tsx
+++ src/components/context-menu/context-menu.tsx
@@ -9,7 +9,10 @@ import {withEventManager} from '../../event';
 
 import styles from '../../styles/style.scss';
 import {KalturaPlayer} from '@playkit-js/kaltura-player-js';
-import {EventManager} from '@playkit-js/playkit-js';
+import {Env, EventManager} from '@playkit-js/playkit-js';
+import {ContextMenuUtils} from './context-menu-utils';
+
+import * as globalStyle from '../../styles/style.scss';
 
 interface ContextMenuProps {
   player: KalturaPlayer;
@@ -24,7 +27,7 @@ const translations = {
 
 function mapStateToProps(state): any {
   return {
-    isFullscreen: state.engine.fullscreen,
+    isFullscreen: state.engine.fullscreen
   };
 }
 
@@ -34,62 +37,83 @@ function _ContextMenu({player, eventManager, copyDebugInfoLabel, isFullscreen}: 
   const isVisibleRef = useRef(isVisible);
 
   useEffect(() => {
+    let touchStartTime: number | null = null;
+
+    const hideContextMenu = (): void => {
+      eventManager!.unlisten(document, 'click', hideContextMenu);
+      setIsVisible(false);
+    };
+
+    const showContextMenu = (e: MouseEvent): void => {
+      if (!ref.current || !(player as any).debugInfo || !document.getElementById(player.config.targetId)?.contains(e.target as Node)) return;
+      if (isVisibleRef.current) {
+        hideContextMenu();
+        return;
+      }
+
+      e.preventDefault();
+
+      const container = document.getElementById(player.config.targetId);
+      if (!container) return;
+
+      const menuWidth = Number((globalStyle as any).contextMenuWidth);
+      const menuHeight = Number((globalStyle as any).contextMenuHeight);
+
+      let posX = e.pageX;
+      let posY = e.pageY;
+
+      if (posX + menuWidth > window.innerWidth) {
+        posX = window.innerWidth - menuWidth;
+      }
+      if (posY + menuHeight > window.innerHeight) {
+        posY = window.innerHeight - menuHeight;
+      }
+
+      ref.current.style.left = posX + 'px';
+      ref.current.style.top = posY + 'px';
+      eventManager!.listen(document, 'click', hideContextMenu);
+      setIsVisible(true);
+    };
+
+    const handleTouchStart = (e: any): void => {
+      touchStartTime = Date.now();
+      e.preventDefault();
+    };
+
+    const handleTouchEnd = (e: MouseEvent): void => {
+      if (touchStartTime === null || (touchStartTime && Date.now() - touchStartTime < 500)) return;
+
+      showContextMenu({
+        target: e.target,
+        pageX: e.pageX,
+        pageY: e.pageY,
+        preventDefault: e.preventDefault.bind(e)
+      } as any as MouseEvent);
+
+      touchStartTime = null;
+    };
+
+    if (!Env.isIOS) {
+      eventManager!.listen(document, 'contextmenu', showContextMenu);
+    } else {
+      eventManager!.listen(document, 'touchstart', handleTouchStart);
+      eventManager!.listen(document, 'touchend', handleTouchEnd);
+    }
+  }, []);
+
+  useEffect(() => {
     isVisibleRef.current = isVisible;
   }, [isVisible]);
 
-  const closeContextMenu = (): void => {
-    eventManager!.unlisten(document, 'click', closeContextMenu);
-    setIsVisible(false);
-  }
-
-  const handleContextMenu = (e: MouseEvent): void => {
-    if (!ref.current || !(player as any).debugInfo || !document.getElementById(player.config.targetId)?.contains(e.target as Node)) return;
-    if (isVisibleRef.current) {
-      closeContextMenu();
-      // use native context menu
-      return;
-    }
-
-    e.preventDefault();
-
-    const menuWidth = ref.current.offsetWidth;
-    const menuHeight = ref.current.offsetHeight;
-
-    let posX = e.pageX;
-    let posY = e.pageY;
-
-    if (posX + menuWidth > window.innerWidth) {
-      posX = window.innerWidth - menuWidth;
-    }
-    if (posY + menuHeight > window.innerHeight) {
-      posY = window.innerHeight - menuHeight;
-    }
-
-    ref.current.style.left = posX + 'px';
-    ref.current.style.top = posY + 'px';
-    eventManager!.listen(document, 'click', closeContextMenu);
-    setIsVisible(true);
-  }
-
-  useEffect(() => {
-    eventManager!.listen(document, 'contextmenu', handleContextMenu);
-  }, []);
-
-  function handleMenuClick(): void {
-    const debugInfo = (player as any).debugInfo;
-    const debugInfoString = JSON.stringify(debugInfo, null, 2);
-    navigator.clipboard.writeText(debugInfoString);
-  }
-
-  const renderContextMenu = () => {
+  const renderContextMenu = (): VNode => {
     return (
       <div ref={ref} className={[styles.contextMenu, isVisible ? '' : styles.hidden].join(' ')} role="menu">
-        <div className={styles.contextMenuItem} onClick={handleMenuClick} role="menuitem">
+        <div className={styles.contextMenuItem} onClick={() => ContextMenuUtils.copyDebugInfoToClipboard(player)} role="menuitem">
           {copyDebugInfoLabel}
         </div>
       </div>
     );
-  }
+  };
 
   return isFullscreen ? renderContextMenu() : createPortal(renderContextMenu(), document.body);
 }
