2b5e296888a5443ba5941536995ea13699776a9f
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2026-01-25 15:29:12 +0200

    feat(FEC-14834): Add a "Video has been removed" message to Player V7

--- src/components/error-overlay/error-message-provider.ts
+++ src/components/error-overlay/error-message-provider.ts
@@ -35,7 +35,9 @@ const errorsMap: Map<number, ErrorDetails> = new Map<number, ErrorDetails>([
   /** SCHEDULED RESTRICTION */
   [17, {title: 'scheduled_restricted_error_title', message: 'scheduled_restricted_error_message'}],
   /** ACCESS CONTROL */
-  [18, {title: 'error_title', message: 'access_control_error_message'}]
+  [18, {title: 'error_title', message: 'access_control_error_message'}],
+  /** DELETED ENTRY */
+  [19, {title: 'media_unavailable_error_title', message: 'deleted_entry_error_message'}]
 ]);
 
 const defaultError: ErrorDetails = {
