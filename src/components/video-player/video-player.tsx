1b7750560576e245f66f327059b8693b1e5c04e1
valentinStoicaK <162963925+valentinStoicaK@users.noreply.github.com>
2026-05-18 14:09:25 +0300

    fix(ADA-3347): Add aria-label to landmark region

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -1,6 +1,7 @@
 import {ResizeWatcher} from '../../utils/resize-watcher';
 import {h, Component, VNode} from 'preact';
 import {connect} from 'react-redux';
+import {withText} from 'preact-i18n';
 import {actions} from '../../reducers/shell';
 import style from '../../styles/style.scss';
 import {bindActions} from '../../utils';
@@ -15,7 +16,8 @@ import {PlayerArea} from '../player-area';
  */
 const mapStateToProps = state => ({
   videoStyles: state.shell.layoutStyles.video,
-  targetId: state.config.targetId
+  targetId: state.config.targetId,
+  sources: state.engine.sources
 });
 
 /**
@@ -25,9 +27,12 @@ const mapStateToProps = state => ({
  * @example <VideoPlayer>...</VideoPlayer>
  * @extends {Component}
  */
+@connect(mapStateToProps, bindActions(actions))
 @withPlayer
 @withEventManager
-@connect(mapStateToProps, bindActions(actions))
+@withText({
+  videoPlayerLabel: 'videoArea.videoPlayer'
+})
 class VideoPlayer extends Component<any, any> {
   _el!: HTMLDivElement;
   _videoResizeWatcher!: ResizeWatcher;
@@ -40,7 +45,7 @@ class VideoPlayer extends Component<any, any> {
    * @memberof VideoPlayer
    */
   shouldComponentUpdate(nextProps: any): boolean {
-    return nextProps.videoStyles !== this.props.videoStyles;
+    return nextProps.videoStyles !== this.props.videoStyles || nextProps.sources !== this.props.sources;
   }
 
   /**
@@ -109,10 +114,12 @@ class VideoPlayer extends Component<any, any> {
    * @memberof VideoPlayer
    */
   render(): VNode<any> {
-    const {videoStyles, targetId} = this.props;
+    const {videoStyles, targetId, sources, videoPlayerLabel} = this.props;
+    const entryName = sources?.metadata?.name;
+    const ariaLabel = entryName || videoPlayerLabel;
 
     return (
-      <div role='region' id={`${targetId}-video`} className={style.videoPlayer} style={videoStyles} ref={this._setRef}>
+      <div role='region' aria-label={ariaLabel} id={`${targetId}-video`} className={style.videoPlayer} style={videoStyles} ref={this._setRef}>
         <PlayerArea name={'VideoContainer'} />
       </div>
     );
