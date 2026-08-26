6dcd5e648ba50fa54617d5d1aacadf8db94bb3e9
Valentin Stoica <162963925+valentinStoicaK@users.noreply.github.com>
2026-08-26 12:32:30 +0300

    Revert "fix(FEC-15083): Add right controls to xs player" (#1199)
    Reverts kaltura/playkit-js-ui#1198

--- src/components/bottom-bar/bottom-bar-utils.ts
+++ src/components/bottom-bar/bottom-bar-utils.ts
@@ -5,11 +5,7 @@ export function filterControlsByPriority(
   currentControlWidth: number,
   lowerPriorityControls: string[][]
 ): string[] {
-  // Add 20% buffer to account for spacing/margins between controls
-  const overflow = currentMinBreakPointWidth - currentBarWidth;
-  const adjustedOverflow = overflow * 1.2;
-
-  const numOfOverflowControls = Math.ceil(adjustedOverflow / currentControlWidth) || 1;
+  const numOfOverflowControls = Math.ceil((currentMinBreakPointWidth - currentBarWidth) / currentControlWidth) || 1;
   const controlsToRemove = lowerPriorityControls.flat().slice(0, numOfOverflowControls);
   const priorityPair: string[] = [...lowerPriorityControls].reverse().find(p => p.length > 1)!;
   if (controlsToRemove[controlsToRemove.length - 1] === priorityPair?.[0]) {
